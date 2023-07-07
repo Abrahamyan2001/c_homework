c5aebec48140f5900a1e713c265032090a733cfb
Abrahamyan2001 <138877511+Abrahamyan2001@users.noreply.github.com>
2023-07-07 18:21:12 +0400

    Add files via upload

---
+++ gorc2_hwork_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+
+	int num1;
+	int num2;
+	
+	printf("Enter your number1 = ");
+	scanf("%d", &num1);
+
+	printf("Enter your number2 = ");
+	scanf("%d", &num2);
+
+	if(num1 > num2){
+		printf("Number1 is more large number than Number2 \n");
+	} else {
+		printf("Number2 is more large number than Number1 \n");
+	}
+}
